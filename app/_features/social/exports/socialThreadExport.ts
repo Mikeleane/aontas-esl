0030cff94f7ba9e24cee25e11b56f80ab0db922d
Mikeleane <servilingual@gmail.com>
2026-02-06 06:20:47 +0100

    Social export: use supported variant (fix adapted mismatch)

--- app/_features/social/exports/socialThreadExport.ts
+++ app/_features/social/exports/socialThreadExport.ts
@@ -354,7 +354,7 @@ export async function exportSocialThreadHtml(opts: ExportOpts): Promise<void> {
       <label>Variant:
         <select id="variantSel">
           <option value="standard">Standard</option>
-          <option value="adapted">Adapted</option>
+          <option value="supported">Supported</option>
         </select>
       </label>
 
